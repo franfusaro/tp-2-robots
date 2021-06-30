379982d3214ffe15838484a33c1ceb0dd90075ab
Franco <ffusaro94@gmail.com>
2021-06-30 01:41:29 -0300

    some changes

--- front-end/helper.js
+++ front-end/helper.js
@@ -2,7 +2,7 @@ var helper = (function() {
   var _helper = {};
 
   _helper.getAddress = function() {
-    return "0x0bC7B0fC1DC8e6da622B0dae12d80F1F9d1be213";
+    return "0x1143A5b2B334129963788F36719A18540Cfe6A06";
   };
 
   return _helper;
