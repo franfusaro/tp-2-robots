44f701eb84d7a862b08292f2e220274b8ed31b1b
Franco <ffusaro94@gmail.com>
2021-07-06 21:41:28 -0300

    Added try catch for crear curso y edit curso

--- front-end/helper.js
+++ front-end/helper.js
@@ -1,4 +1,4 @@
-var helper = (function() {
+var helper = (function () {
     var _helper = {};
     var contractAddress = "0x905850E4f90ce4488E43c6D3E8d4cA3eBd1f71d9";
     var contract;
@@ -27,74 +27,84 @@ var helper = (function() {
     };
 
     _helper.createCurso = function (id, nombre, profesor, creditos, correlativas, onReceipt, onError) {
-        _helper.contract.methods.createCurso(id, nombre, profesor, creditos, correlativas)
-            .send({ from: _helper.userAccount })
-            .on("receipt", function (receipt) {
-                onReceipt(receipt);
-            })
-            .on("error", function (error) {
-                onError(error);
-            });
+        try {
+            _helper.contract.methods.createCurso(id, nombre, profesor, creditos, correlativas)
+                .send({ from: _helper.userAccount })
+                .on("receipt",
+                    function (receipt) {
+                        onReceipt(receipt);
+                    })
+                .on("error",
+                    function (error) {
+                        onError(error);
+                    });
+        } catch (ex) {
+            alert(`Alguno de los valores ingresados no es correcto: ${ex}`);
+        }
     };
 
     _helper.editCurso = function (id, nombre, profesor, creditos, correlativas, onReceipt, onError) {
-        _helper.contract.methods.modifyCurso(id, nombre, profesor, creditos, correlativas)
-            .send({ from: _helper.userAccount })
-            .on("receipt", function (receipt) {
-                onReceipt(receipt);
-            })
-            .on("error", function (error) {
-                onError(error);
-            });
-    };
+        try {
+            _helper.contract.methods.modifyCurso(id, nombre, profesor, creditos, correlativas)
+                .send({ from: _helper.userAccount })
+                .on("receipt", function (receipt) {
+                    onReceipt(receipt);
+                })
+                .on("error", function (error) {
+                    onError(error);
+                });
 
-    _helper.getCursos = function (onSuccess) {
-        // Returns a list of cursos IDs
-        _helper.contract.methods.listCursos().call()
-            .then(function(ids) {
-                onSuccess(ids);
-            })
-            .catch(err => alert(err));
-    }
+        } catch (ex) {
+            alert(`Alguno de los valores ingresados no es correcto: ${ex}`);
+        };
 
-    _helper.getCursoDetails = function(id, onSuccess) {
-        _helper.contract.methods.getCurso(id).call()
-            .then(function (curso) { onSuccess(curso); })
-            .catch(err => alert(err));
-    }
+        _helper.getCursos = function (onSuccess) {
+            // Returns a list of cursos IDs
+            _helper.contract.methods.listCursos().call()
+                .then(function (ids) {
+                    onSuccess(ids);
+                })
+                .catch(err => alert(err));
+        }
 
-    _helper.aprobarSoloCursada = function (address, idCurso, onReceipt, onError) {
-        _helper.contract.methods.asignarAprobacionSoloCursada(address,idCurso)
-            .send({ from: _helper.userAccount })
-            .on("receipt", function (receipt) {
-                onReceipt(receipt);
-            })
-            .on("error", function (error) {
-                onError(error);
-            });
-    }
+        _helper.getCursoDetails = function (id, onSuccess) {
+            _helper.contract.methods.getCurso(id).call()
+                .then(function (curso) { onSuccess(curso); })
+                .catch(err => alert(err));
+        }
 
-    _helper.aprobarFinal = function (address, idCurso, nota, onReceipt, onError) {
-        _helper.contract.methods.asignarAprobacionFinalCursada(address,idCurso,nota)
-            .send({ from: _helper.userAccount })
-            .on("receipt", function (receipt) {
-                onReceipt(receipt);
-            })
-            .on("error", function (error) {
-                onError(error);
-            });
-    }
+        _helper.aprobarSoloCursada = function (address, idCurso, onReceipt, onError) {
+            _helper.contract.methods.asignarAprobacionSoloCursada(address, idCurso)
+                .send({ from: _helper.userAccount })
+                .on("receipt", function (receipt) {
+                    onReceipt(receipt);
+                })
+                .on("error", function (error) {
+                    onError(error);
+                });
+        }
 
-    _helper.getCusadaAprobada = function(id, address, onSuccess) {
-        _helper.contract.methods.getCusadaAprobadas(id, address).call()
-            .then(function(cursada) { onSuccess(cursada); });
-    }
+        _helper.aprobarFinal = function (address, idCurso, nota, onReceipt, onError) {
+            _helper.contract.methods.asignarAprobacionFinalCursada(address, idCurso, nota)
+                .send({ from: _helper.userAccount })
+                .on("receipt", function (receipt) {
+                    onReceipt(receipt);
+                })
+                .on("error", function (error) {
+                    onError(error);
+                });
+        }
 
-    _helper.getCreditosAlumno = function(alumno, onSuccess) {
-        _helper.contract.methods.getCreditosAlumno(alumno).call()
-            .then(function (creditos) { onSuccess(creditos); });
-    }
+        _helper.getCusadaAprobada = function (id, address, onSuccess) {
+            _helper.contract.methods.getCusadaAprobadas(id, address).call()
+                .then(function (cursada) { onSuccess(cursada); });
+        }
+
+        _helper.getCreditosAlumno = function (alumno, onSuccess) {
+            _helper.contract.methods.getCreditosAlumno(alumno).call()
+                .then(function (creditos) { onSuccess(creditos); });
+        }
 
 
-    return _helper;
-})();
+        return _helper;
+    }) ();
